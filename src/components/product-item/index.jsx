3ce6f985d06ed81cdde7fb565cd470633f14622c
Cézar Bezerra <cezar30santos@gmail.com>
2024-02-22 11:08:53 -0300

    feat: implements product-item component

---
+++ src/components/product-item/index.jsx
@@ -0,0 +1,22 @@
+import { BsCartPlus } from "react-icons/bs";
+import CustomButton from "../custom-button/index";
+import * as Styles from "./styles";
+
+const ProductItem = ({ product }) => {
+  return (
+    <Styles.ProductContainer>
+      <Styles.ProductImage imageUrl={product.imageUrl}>
+        <CustomButton startIcon={<BsCartPlus />}>
+          Adicionar ao carrinho
+        </CustomButton>
+      </Styles.ProductImage>
+
+      <Styles.ProductInfo>
+        <p>{product.name}</p>
+        <p>R${product.price}</p>
+      </Styles.ProductInfo>
+    </Styles.ProductContainer>
+  );
+};
+
+export default ProductItem;
