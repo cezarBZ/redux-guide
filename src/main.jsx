5bd4d817ed60d891bcde3e9e2c7c7979e3251d54
Cézar Bezerra <cezar30santos@gmail.com>
2024-02-22 12:05:55 -0300

    feat: create the shopping cart reducer and configure redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import ResetStyles from './styles/resets.js'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import ResetStyles from "./styles/resets.js";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <ResetStyles /> */}
-    <App />
-  </React.StrictMode>,
-)
+    <ResetStyles />
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
---
+++ src/redux/cart/action-types.js
@@ -0,0 +1,5 @@
+export const CartActionTypes = {
+  ADD_PRODUCT: "cart/addProduct",
+  REMOVE_PRODUCT: "cart/removeProduct",
+  DECREASE_PRODUCT_QUANTITY: "cart/decreaseProductQuantity",
+};
